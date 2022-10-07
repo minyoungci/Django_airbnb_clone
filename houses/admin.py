102bd7e3ba724e9749b5cc4e4b489b89b369408e
minyoungci <dbssus123@gmail.com>
2022-10-07 13:53:27 +0900

    admin 살짝 수정

--- houses/admin.py
+++ houses/admin.py
@@ -5,9 +5,19 @@ from .models import House
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
 
+    fields = (
+        "name",
+        "address",
+        ("price_per_night", "pets_allowed"),
+    )
+
     list_display = ["name", "price_per_night", "address", "pet_allowed"]
     list_filter = ["price_per_night", "pet_allowed"]
-    search_fields = ["address__startswith"]
+    search_fields = [
+        "address",
+    ]
+    list_display_links = ("name", "address")
+    list_editable = ("pet_allowed",)
 
 
 # Register your models here.
