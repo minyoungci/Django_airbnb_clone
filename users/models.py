27a07666d52a89970de0c4979b8416564c01a849
minyoungci <dbssus123@gmail.com>
2022-10-13 15:47:47 +0900

    users admin

--- users/models.py
+++ users/models.py
@@ -3,7 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    first_name = models.CharField(max_length=150, editable=False)
+    last_name = models.CharField(max_length=150, editable=False)
+    name = models.CharField(max_length=150, default="")
+    is_host = models.BooleanField(default=False)
 
 
 # Create your models here.
