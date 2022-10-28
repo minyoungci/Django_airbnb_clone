2a56b48373776a70011927b48dc5ddd59d28a063
minyoungci <dbssus123@gmail.com>
2022-10-28 16:40:54 +0900

    Foreign key 사용법 + admin 패널 에러 해결해야함

--- houses/admin.py
+++ houses/admin.py
@@ -8,7 +8,7 @@ class HouseAdmin(admin.ModelAdmin):
     fields = (
         "name",
         "address",
-        ("price_per_night", "pets_allowed"),
+        ("price_per_night", "pet_allowed"),
     )
 
     list_display = ["name", "price_per_night", "address", "pet_allowed"]
--- houses/models.py
+++ houses/models.py
@@ -1,4 +1,6 @@
+from tkinter import CASCADE
 from django.db import models
+from django.conf import settings
 
 
 class House(models.Model):
@@ -6,10 +8,18 @@ class House(models.Model):
     """Model Definition for Houses"""
 
     name = models.CharField(max_length=140)
-    price_per_night = models.PositiveBigIntegerField()
+    price_per_night = models.PositiveBigIntegerField(
+        verbose_name="Price", help_text="Positive Numvers Only"
+    )
     description = models.TextField()
     address = models.CharField(max_length=140)
-    pet_allowed = models.BooleanField(default=True)
+    pet_allowed = models.BooleanField(
+        default=True,
+        verbose_name="Pets Allowed?",
+        help_text="Does this house allow pests?",
+    )
+
+    owner = models.ForeignKey("users.User", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
