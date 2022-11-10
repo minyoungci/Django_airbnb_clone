655ead1634c8c6891ed85c43830566a2557dc6e1
minyoungci <dbssus123@gmail.com>
2022-11-10 20:59:37 +0900

    bookings app

---
+++ bookings/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Booking
+
+
+@admin.register(Booking)
+class Booking(admin.ModelAdmin):
+    list_display = (
+        "kind",
+        "user",
+        "room",
+        "experiences",
+        "check_in",
+        "check_out",
+        "experience_time",
+        "guests",
+    )
+
+    list_filter = ("kind",)
