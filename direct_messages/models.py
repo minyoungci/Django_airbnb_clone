2cf4bbd1efcc273d42a47642124e51ab7421405d
minyoungci <dbssus123@gmail.com>
2022-11-14 19:50:15 +0900

    Messages app

---
+++ direct_messages/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from common.models import CommonModel
+
+
+class ChattingRoom(CommonModel):
+
+    users = models.ManyToManyField(
+        "users.User",
+    )
+
+    def __str__(self):
+        return "Chatting Room."
+
+
+class Message(CommonModel):
+
+    text = models.TextField()
+    user = models.ForeignKey(
+        "users.User",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
+
+    room = models.ForeignKey(
+        "direct_messages.ChattingRoom",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return f"{self.user} says: {self.text}"
