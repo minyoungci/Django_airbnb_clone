9f5a6cff4ef717815c8cd829cb90be18b44c6629
minyoungci <dbssus123@gmail.com>
2022-12-27 13:57:02 +0900

    RestFramework Serializer

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Category
+from .serializers import CategorySerializer
 
-# Create your views here.
+
+@api_view()
+def categories(request):
+
+    all_categories = Category.objects.all()
+    serializer = CategorySerializer(all_categories, many=True)
+    return Response(
+        {
+            "ok": True,
+            "categories": serializer.data,
+        }
+    )
--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ from rooms import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("rooms/", include("rooms.urls")),
+    path("categories/", include("categories.urls")),
 ]
