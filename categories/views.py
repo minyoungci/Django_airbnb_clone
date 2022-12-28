dec9f0f52ac236f288719bab6c63b5ddb902f2b1
minyoungci <dbssus123@gmail.com>
2022-12-28 15:43:20 +0900

    DELETE

--- categories/views.py
+++ categories/views.py
@@ -1,4 +1,6 @@
 from rest_framework.decorators import api_view
+from rest_framework.exceptions import NotFound
+from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.response import Response
 from .models import Category
 from .serializers import CategorySerializer
@@ -15,14 +17,39 @@ def categories(request):
     elif request.method == "POST":
         serializer = CategorySerializer(data=request.data)
         if serializer.is_valid():
-            return Response({"created_at": True})
+            new_category = serializer.save()
+            return Response(
+                CategorySerializer(new_category).data,
+            )
 
         else:
             return Response(serializer.errors)
 
 
-@api_view()
+@api_view(["GET", "PUT", "DELETE"])
 def category(request, pk):
-    category = Category.objects.get(pk=pk)
-    serializer = CategorySerializer(category)
-    return Response(serializer.data)
+
+    try:
+        category = Category.objects.get(pk=pk)
+    except Category.DoesNotExist:
+        raise NotFound
+
+    if request.method == "GET":
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+
+    elif request.method == "PUT":
+        serializer = CategorySerializer(
+            category,
+            data=request.data,
+            partial=True,
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+        else:
+            return Response(serializer.errors)
+
+    elif request.method == "DELETE":
+        category.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,6 @@ from rooms import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("rooms/", include("rooms.urls")),
-    path("categories/", include("categories.urls")),
+    path("api/v1/rooms/", include("rooms.urls")),
+    path("api/v1/categories/", include("categories.urls")),
 ]
