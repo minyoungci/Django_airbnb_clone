45787d949781bfe749d594415d292f811ccd32f9
minyoungci <dbssus123@gmail.com>
2023-01-11 18:33:55 +0900

    Reverse Serialization

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import Amenity, Room
 from categories.serializers import CategorySerializer
 from users.serializers import TinyUserSerializer
 
 
-class AmenitySerializer(ModelSerializer):
+class AmenitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Amenity
         fields = (
@@ -13,24 +14,39 @@ class AmenitySerializer(ModelSerializer):
         )
 
 
-class RoomDetailSerializer(ModelSerializer):
+class RoomDetailSerializer(serializers.ModelSerializer):
 
     owner = TinyUserSerializer(read_only=True)
     amenities = AmenitySerializer(read_only=True, many=True)
     category = CategorySerializer(read_only=True)
 
+    rating = serializers.SerializerMethodField()
+    is_owner = serializers.SerializerMethodField()
+
     class Meta:
         model = Room
         fields = "__all__"
 
+    def get_rating(self, room):
+        return room.rating()
 
-class RoomListSerializer(ModelSerializer):
+    def get_is_owner(self, room):
+        request = self.context["request"]
+        return room.owner == request.user
+
+
+class RoomListSerializer(serializers.ModelSerializer):
+
+    rating = serializers.SerializerMethodField
+    is_owner = serializers.SerializerMethodField()
+
     class Meta:
         model = Room
-        fields = (
-            "pk",
-            "name",
-            "country",
-            "city",
-            "price",
-        )
+        fields = ("pk", "name", "country", "city", "price", "rating", "is_owner")
+
+    def get_rating(self, room):
+        return room.rating()
+
+    def get_is_owner(self, room):
+        request = self.context["request"]
+        return room.owner == request.user
