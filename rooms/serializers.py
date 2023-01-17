17acdb76294b1f2e93034b35038a9543a0ab055b
minyoungci <dbssus123@gmail.com>
2023-01-17 11:52:27 +0900

    is_liked()

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -5,6 +5,7 @@ from reviews.serializers import ReviewSerializer
 from categories.serializers import CategorySerializer
 from users.serializers import TinyUserSerializer
 from medias.serializers import PhotoSerializer
+from wishlists.models import Wishlist
 
 
 class AmenitySerializer(serializers.ModelSerializer):
@@ -24,6 +25,7 @@ class RoomDetailSerializer(serializers.ModelSerializer):
 
     rating = serializers.SerializerMethodField()
     is_owner = serializers.SerializerMethodField()
+    is_liked = serializers.SerializerMethodField()
     photos = PhotoSerializer(many=True, read_only=True)
 
     class Meta:
@@ -36,6 +38,13 @@ class RoomDetailSerializer(serializers.ModelSerializer):
     def get_is_owner(self, room):
         request = self.context["request"]
         return room.owner == request.user
+
+    def get_is_liked(self, room):
+        request = self.context["request"]
+        return Wishlist.objects.filter(
+            user=request.user,
+            rooms__id=room.pk,
+        ).exists()
 
 
 class RoomListSerializer(serializers.ModelSerializer):
