6fc27ed4d208910ec548199c4eaaed57a225a19c
minyoungci <dbssus123@gmail.com>
2023-01-25 12:43:39 +0900

    log in - log out

--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import authenticate, login, logout
+
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
@@ -74,3 +76,30 @@ class ChangePassword(APIView):
             return Response(status=status.HTTP_200_OK)
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogIn(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        if not username or not password:
+            raise ParseError
+        user = authenticate(
+            request,
+            username=username,
+            password=password,
+        )
+        if user:
+            login(request, user)
+            return Response({"ok": "Welcome"})
+        else:
+            return Response({"error": "wrong passowrd"})
+
+
+class LogOut(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        logout(request)
+        return Response({"ok": "logout successful"})
