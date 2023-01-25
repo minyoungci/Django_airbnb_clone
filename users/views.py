7dcc5ce0737ee6ef3b4946a29132d380e17a03f7
minyoungci <dbssus123@gmail.com>
2023-01-25 11:51:04 +0900

    Create User

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework.exceptions import ParseError
 from rest_framework.permissions import IsAuthenticated
 
 from . import serializers
@@ -25,6 +26,22 @@ class Me(APIView):
 
         if serializer.is_valid():
             user = serializer.save()
+            serializer = serializers.PrivateUserSerializer(user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class Users(APIView):
+    def post(self, request):
+        password = request.data.get("password")
+        if not password:
+            raise ParseError
+        serializer = serializers.PrivateUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            user.set_password(password)
+            user.save()
             serializer = serializers.PrivateUserSerializer(user)
             return Response(serializer.data)
         else:
